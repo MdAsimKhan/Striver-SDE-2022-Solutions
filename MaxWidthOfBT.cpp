ce2dacf82c898bfb8c1b46cbdac117ae3bba8a03
Mohammad Asim Khan <ak4514768@gmail.com>
2022-07-21 23:40:42 +0530

    21 July

--- MaxWidthOfBT.cpp
+++ MaxWidthOfBT.cpp
@@ -1,13 +1,13 @@
 int widthOfBinaryTree(TreeNode* root) {
         int ans=0;
-        queue<pair<TreeNode*, int>> q;
+        queue<pair<TreeNode*, long>> q;
         q.push({root, 0});
         while(!q.empty()){
             int sz=q.size();
-            int f,l;
-            int min_ind=q.front().second;//int val in q
+            int f=0,l=0;
+            long min_ind=q.front().second;//int val in q
             for(int i=0;i<sz;i++){
-                int cur_ind=q.front().second - min_ind;
+                long cur_ind=q.front().second - min_ind;
                 TreeNode* node=q.front().first;
                 q.pop();
                 if(i==0) f=cur_ind;
@@ -18,4 +18,4 @@ int widthOfBinaryTree(TreeNode* root) {
             ans=max(ans,l-f+1);
         }
         return ans;
-}
+    }
