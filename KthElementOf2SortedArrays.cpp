e1ab071130a200ba0e977c348bb76d46eaf7db83
Mohammad Asim Khan <ak4514768@gmail.com>
2022-08-15 23:01:00 +0530

    Updated - 15 August

--- KthElementOf2SortedArrays.cpp
+++ KthElementOf2SortedArrays.cpp
@@ -19,3 +19,45 @@ int kthElement(int nums1[], int nums2[], int n, int m, int k)
         }
         return 0;
     }
+
+
+//OPTIMISED
+
+int kth(int *arr1, int *arr2, int *end1, int *end2, int k) 
+    { 
+        if (arr1 == end1) 
+            return arr2[k]; 
+        if (arr2 == end2) 
+            return arr1[k]; 
+        int mid1 = (end1 - arr1) / 2; 
+        int mid2 = (end2 - arr2) / 2; 
+    
+        // taking mid1+mid2 size from both the arr
+        if (mid1 + mid2 < k) 
+        //arr1[mid1] > arr2[mid2] and len < k => kth elem would be greater than arr2[mid2] at least,
+        //so move forward and no need to check smaller elemens so take part of unchecked arr2
+        //so we have passed mid2+1 nos, thus we need to find (k-mid2-1)th no
+        { 
+            if (arr1[mid1] > arr2[mid2]) 
+                return kth(arr1, arr2 + mid2 + 1, end1, end2, 
+                    k - mid2 - 1); 
+            else
+                return kth(arr1 + mid1 + 1, arr2, end1, end2, 
+                    k - mid1 - 1); 
+        } 
+        else
+        //>= case => shift back with same arrs, kth can be anything from all the elems
+        { 
+            if (arr1[mid1] > arr2[mid2]) 
+                return kth(arr1, arr2, arr1 + mid1, end2, k); 
+            else
+                return kth(arr1, arr2, end1, arr2 + mid2, k); 
+        } 
+    } 
+    
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        //k-1 for 0-index
+        //passing iterators so as to easily pass portions of arrs in kth func
+        return kth(arr1, arr2, arr1+n, arr2+m, k-1);
+    }
