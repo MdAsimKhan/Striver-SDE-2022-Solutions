9c8d0b683b418e734afb2da94052977e41f70cfc
Mohammad Asim Khan <ak4514768@gmail.com>
2022-08-17 21:53:56 +0530

    Added Comments - 17 August

--- TwoSumIVwithBST.cpp
+++ TwoSumIVwithBST.cpp
@@ -7,13 +7,14 @@ public:
         pushAll(root);
     }
     
-    int next() {
+    int next(){
         TreeNode* x = st.top();
         st.pop();
+        //push all right nodes as lefts are pushed
         if(!rev) pushAll(x->right);
+        //push all left nodes as right are pushed
         else pushAll(x->left);
         return x->val;
-
     }
     
     bool hasNext() {
@@ -23,18 +24,24 @@ public:
     void pushAll(TreeNode* root){
         while(root){
             st.push(root);
+            //push all left nodes from root
             if(!rev) root=root->left;
+            //push all right nodes from root
             else root=root->right;
         }
     }
 };
+
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
+        //inorder of bst is a sorted arr
         BSTIterator l(root,false);
         BSTIterator r(root,true);
+        //i and j point to 1st and last index of tree
         int i = l.next();
         int j = r.next();
+        //apply bs
         while(i<j){
             if(i+j==k) return true;
             else if(i+j<k) i=l.next();
